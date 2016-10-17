9d2be773c328e9d6f80dbcfd82161b046e2ea366
Andrew Bulychev <bulychev@hydra.jinr.ru>
2016-10-17 12:41:00 +0300

    Updates in dependencies installation script

--- hydra_install_deps.sh
+++ hydra_install_deps.sh
@@ -8,30 +8,33 @@ module add gcc-4.8
 # Install PETSc
 cd
 mkdir petsc
-mkdir petsc/debug
+#mkdir petsc/debug
 mkdir petsc/opt
 ### get source
 #git clone -b maint https://bitbucket.org/petsc/petsc petsc
 wget http://ftp.mcs.anl.gov/pub/petsc/release-snapshots/petsc-lite-3.6.2.tar.gz
-tar -xvzf petsc-lite-3.6.2.tar.gz
-cd petsc-3.6.2
 
 ### compile debug-version
-PETSC_FLAGS="--with-cc=mpicc --with-cxx=mpic++ --with-fc=0 --with-clanguage=cxx \
-        --download-hypre=yes \
-        --download-metis=yes --download-parmetis=yes  --download-superlu_dist" 
-        #--download-mumps=yes --with-scalapack --with-fc=gfortran \
-./configure ${PETSC_FLAGS} --prefix=${HOME}/petsc/debug --with-debugging=1
-make PETSC_DIR=${HOME}/petsc-3.6.2 PETSC_ARCH=arch-linux2-cxx-debug all
-make PETSC_DIR=${HOME}/petsc-3.6.2 PETSC_ARCH=arch-linux2-cxx-debug test
-make install DESTDIR=${HOME}/petsc/debug
+#tar -xvzf petsc-lite-3.6.2.tar.gz
+#cd petsc-3.6.2
+#PETSC_FLAGS="--with-cc=mpicc --with-cxx=mpic++ --with-fc=0 --with-clanguage=cxx \
+#        --download-hypre=yes \
+#        --download-metis=yes --download-parmetis=yes  --download-superlu_dist" 
+#        #--download-mumps=yes --with-scalapack --with-fc=gfortran \
+#./configure ${PETSC_FLAGS} --prefix=${HOME}/petsc/debug --with-debugging=1
+#make PETSC_DIR=${HOME}/petsc-3.6.2 PETSC_ARCH=arch-linux2-cxx-debug all
+#make PETSC_DIR=${HOME}/petsc-3.6.2 PETSC_ARCH=arch-linux2-cxx-debug test
+#make install DESTDIR=${HOME}/petsc/debug
 #make distclean
-cd ..
-rm -rf petsc-3.6.2
+#cd ..
+#rm -rf petsc-3.6.2
 
 ### compile production-version
 tar -xvzf petsc-lite-3.6.2.tar.gz
 cd petsc-3.6.2
+PETSC_FLAGS="--with-cc=mpicc --with-cxx=mpic++ --with-fc=0 --with-clanguage=cxx \
+        --download-hypre=yes \
+        --download-metis=yes --download-parmetis=yes  --download-superlu_dist" 
 ./configure ${PETSC_FLAGS} --prefix=${HOME}/petsc/opt --with-debugging=0
 make PETSC_DIR=${HOME}/petsc-3.6.2 PETSC_ARCH=arch-linux2-cxx-opt all
 make PETSC_DIR=${HOME}/petsc-3.6.2 PETSC_ARCH=arch-linux2-cxx-opt test
@@ -48,16 +51,3 @@ cd hdf5
 yumdownloader hdf5-openmpi-devel.x86_64 &&
 yumdownloader hdf5-openmpi.x86_64 &&
 for x in *.rpm; do rpm2cpio $x | cpio -idv ; done
-
-
-# Install OCE
-cd
-mkdir OCE
-cd OCE
-yumdownloader OCE-devel.x86_64 &&
-yumdownloader OCE-draw.x86_64 &&
-yumdownloader OCE-foundation.x86_64 &&
-yumdownloader OCE-modeling.x86_64 &&
-yumdownloader OCE-ocaf.x86_64 &&
-yumdownloader OCE-visualization.x86_64 &&
-for x in *.rpm; do rpm2cpio $x | cpio -idv; done
